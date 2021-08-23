9f05eaac631be638450232d49265ffcaf87fd765
Maxime <maximelagresle@hotmail.fr>
2021-08-22 20:07:46 -0400

    improved sql query style

--- customers.sql
+++ customers.sql
@@ -1,37 +1,23 @@
-with all_transactions as (
-    select *
-    from `analytics-engineers-club.coffee_shop.orders` as o 
-    left join `analytics-engineers-club.coffee_shop.customers` as c
-    on o.customer_id = c.id
+with customers as (
+    select * from `analytics-engineers-club.coffee_shop.customers`
 ),
 
-first_order as (
-    select 
-    customer_id
-    , min(created_at) as first_order_at
-    from all_transactions
-    group by 1
+orders as (
+    select * from `analytics-engineers-club.coffee_shop.orders`
 ),
 
-number_of_orders as (
-    select 
-    customer_id
-    , count(*) as number_of_orders
-    from all_transactions 
-    group by 1
+final as (
+    select 
+        customers.id as customer_id
+        , customers.name
+        , customers.email
+        , MIN(orders.created_at) AS first_order_at
+        , COUNT(*) AS number_of_orders 
+    from customers 
+    left join orders on customers.id = orders.customer_id
+    group by 1, 2, 3
+    order by 4
+    LIMIT 5
 )
 
-select 
-    t.customer_id
-    , t.name
-    , t.email
-    , fo.first_order_at 
-    , nor.number_of_orders 
-from all_transactions t
-left join first_order fo 
-on t.customer_id = fo.customer_id
-left join number_of_orders nor 
-on t.customer_id = nor.customer_id
-group by 1, 2, 3, 4, 5
-order by 4
-limit 5
+select * from final
